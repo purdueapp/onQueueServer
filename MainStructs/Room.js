0b633bbc4b7a62d89dc43c8b6a37b215c81b3157
Jerosik <yoon161@purdue.edu>
2020-02-18 17:52:14 -0500

    Initialized Room class with room id

--- MainStructs/Room.js
+++ MainStructs/Room.js
@@ -1,1 +1,20 @@
-// Room struct will be created here
+// // Room struct will be created here
+/*
+var Room = {
+    ID           string              `json:"id"`
+	Client       *spotify.Client     `json:"-"`
+	Host         spotify.PrivateUser `json:"host"`
+	HostSocketId string              `json:"-"`
+	Queue        []spotify.FullTrack `json:"queue"`
+	CurrentState string              `json:"state"`
+	Mutex        sync.Mutex          `json:"-"`
+	Server       *socketio.Server    `json:"-"`
+	Size    int                 `json:"size"`
+};
+*/
+class Room {
+    constructor (id) {
+        this.id = id;
+	// Server       *socketio.Server    `json:"-"`
+    }
+}
