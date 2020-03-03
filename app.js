cdccef944e31c5aaf8e9626019bb6ec1077ca035
Jerosik <yoon161@purdue.edu>
2020-03-03 15:32:41 -0500

    Set up basic express server with two events

--- app.js
+++ app.js
@@ -1,59 +1,55 @@
-/**
- * This is an example of a basic node.js script that performs
- * the Authorization Code oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
- */
-
-let express = require('express'); // Express web server framework
-let request = require('request'); // "Request" library
-let cors = require('cors');
-let querystring = require('querystring');
+// Setup basic express server
+const express = require('express');
+const cors = require('cors');
 let cookieParser = require('cookie-parser');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 7374;
 require('dotenv').config();
 
-let client_id = process.env.CLIENT_ID; // Your client id
-let client_secret = process.env.CLIENT_SECRET; // Your secret
-let redirect_uri = 'http://data.cs.purdue.edu:7374/callback'; // Your redirect uri
-
-let app = express();
-let http = require('http').createServer(app);
-let io = require('socket.io')(http);
-
-/*app.use(express.static(__dirname + '/public'))
+// Routing
+app.use(express.static(__dirname + '/public'))
    .use(cors())
-   .use(cookieParser()); */
+   .use(cookieParser());
 
-app.get("/json", (req, res) => {
-  res.json({message: "Hello world"});
+// HTTP server listen
+http.listen(port, function(){
+  console.log('Server listening at port %d', port);
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+// HTML file for testing
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
 
-// Listen on the 'connection' event for incoming sockets
-io.on('connection', function(socket){
+// Handle event for creating a room
+function createRoom(packet) {
+
+}
+
+// Handle event for joining a room
+function joinRoom(packet) {
+
+}
+
+// Fired upon connection with client
+io.on('connection', function(socket) {
   console.log('User connected');
 
-  // Listen to 'chat message' event from client
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+  socket.on('create room', createRoom);
+  socket.on('join room', joinRoom);
 
-    // send a message to everyone except for a certain emitting socket
-    socket.broadcast.emit('Message was recieved.');
-    // send the message to everyone, including the sender
-    io.emit('chat message', msg);
-  });
-
-  // Each socket fires a 'disconnect' event as well
-  socket.on('disconnect', function(){
-    console.log('User disconnected');
-  });
-});
-
-http.listen(7374, function(){
-  console.log('Listening on 7374');
+  // // Listen to 'chat message' event from client
+  // socket.on('chat message', function(msg){
+  //   console.log('message: ' + msg);
+  //   // send a message to everyone except for a certain emitting socket
+  //   socket.broadcast.emit('Message was recieved.');
+  //   // send the message to everyone, including the sender
+  //   io.emit('chat message', msg);
+  // });
+  // // Each socket fires a 'disconnect' event as well
+  // socket.on('disconnect', function(){
+  //   console.log('User disconnected');
+  // });
 });
