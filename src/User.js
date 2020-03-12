02d088ae410843281e2e7ba3055ff9b23ffaf9a8
ericzhvng <zhan2575@purdue.edu>
2020-03-11 23:03:01 -0400

    Revised role structures and clarified documentation

--- src/User.js
+++ src/User.js
@@ -1,8 +1,8 @@
 module.exports = {
   newUser: function(socket, data) {
     var user = {
-      "ID": 0, //import later from Spotify
-      "Role": 0, //0 for Admin, 1 for DJ, 2 for Listener
+      "ID": 0, //import later from Spotify, get Spotify ID if signed in, otherwise generate uniqueID
+      "Role": 0, //0 for Admin, 1 for DJ, 2 for Listener, 3 for Host
     }
 
     console.log('New user created: ' + user.ID);
